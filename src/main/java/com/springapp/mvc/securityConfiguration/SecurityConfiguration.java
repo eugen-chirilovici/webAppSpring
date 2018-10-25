e47c3cbf1a7a7cf17658343a0729c3be35f5c368
Chirilovici Eugeniu <eugen.chirilovici@endava.com>
2018-10-25 09:48:44 +0300

    work version web app security

--- src/main/java/com/springapp/mvc/securityConfiguration/SecurityConfiguration.java
+++ src/main/java/com/springapp/mvc/securityConfiguration/SecurityConfiguration.java
@@ -1,11 +1,11 @@
 package com.springapp.mvc.securityConfiguration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Mila Bezaliuc").password("endava").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("Mila Bezaliuc").password("endava").roles("ADMIN","USER");
         auth.inMemoryAuthentication().withUser("Petru Covaliov").password("endava").roles("USER");
         auth.inMemoryAuthentication().withUser("Maxim Ustimov").password("endava").roles("USER");
     }
@@ -22,15 +22,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/","/allusers","/login").permitAll()
-//                    .antMatchers("/allusers").access("hasRole('USER') and hasRole('ADMIN')")
-                    .antMatchers("/allusers/{gender}").access("hasRole('ADMIN')")
-                    .and().formLogin().loginPage("/login")
+                .antMatchers("/"," /login").permitAll()
+                    .antMatchers("/allusers").access("hasRole('USER') or hasRole('ADMIN')")
+                .antMatchers("/allusers/{gender}").access("hasRole('ADMIN')")
+                .and()
+                .formLogin().loginPage("/login")
                 .defaultSuccessUrl("/allusers")
-                .failureUrl("/error")
-                .usernameParameter("username").passwordParameter("password")
-                .and().csrf().disable();
-//                .exceptionHandling().accessDeniedPage("/");
+                .failureUrl("/")
+                .usernameParameter("name").passwordParameter("password")
+                .and().csrf().disable()
+                .exceptionHandling().accessDeniedPage("/allusers");
 
     }
 }
