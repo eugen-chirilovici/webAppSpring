c0445742416a63d85ad9b1747ed070077def1188
Iulian Cuciuc <iulian.cuciuc@endava.com>
2020-04-01 16:09:54 +0300

    add some new fields to the User class

--- src/main/java/com/springapp/mvc/model/User.java
+++ src/main/java/com/springapp/mvc/model/User.java
@@ -4,6 +4,8 @@ public class User {
     private Long userId;
     private String firstName;
     private String lastName;
+    private int age;
+    private String hobby;
     private Long credentialsId;
 
     public User(String firstName, String lastName, Long credentialsId) {
@@ -16,6 +18,15 @@ public class User {
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.credentialsId = credentialsId;
+    }
+
+    public User(Long userId, String firstName, String lastName, int age, String hobby, Long credentialsId) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.hobby = hobby;
         this.credentialsId = credentialsId;
     }
 
@@ -49,5 +60,21 @@ public class User {
 
     public void setCredentialsId(Long credentialsId) {
         this.credentialsId = credentialsId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
     }
 }
