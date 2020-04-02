26a7e74557fe4aef4595044a877ad86febaa9e58
Nichita Ganja <Nichita.Ganja@endava.com>
2020-04-02 18:19:01 +0300

    Fixed invalid id deleting validation

--- src/main/java/com/springapp/mvc/controller/UserController.java
+++ src/main/java/com/springapp/mvc/controller/UserController.java
@@ -72,7 +72,7 @@ public class UserController {
     @RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
     public String deleteUser(@ModelAttribute("deleteUserDTO") DeleteUserDTO deleteUserDTO) {
 
-        if(deleteUserDTO.getDeletedUserId() < 1 || deleteUserDTO.getDeletedUserId() > userService.getAllUsers().size())
+        if(deleteUserDTO.getDeletedUserId() < 1 || deleteUserDTO.getDeletedUserId() > userService.getAllUsers().size() - 1)
             return "redirect:/error";
         userService.removeUserById(deleteUserDTO);
         return "redirect:/allusers";
