41bda0d64160ebb2b35c8c73fc101c942f94c2e9
Dumitru Crudu <Dumitru.Crudu@endava.com>
2020-04-29 12:29:08 +0300

    SpringBoot V2

--- src/main/java/com/springapp/mvc/model/enums/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.springapp.mvc.model.enums;
-
-public enum RoleType {
-    ROLE_ADMIN,
-    ROLE_USER,
-}
--- src/test/java/app/mvc/controller/RegisterControllerTest.java
+++ src/test/java/app/mvc/controller/RegisterControllerTest.java
@@ -9,17 +9,35 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+
+
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 class RegisterControllerTest {
+
+    @Mock
+    private UserService mockUserService;
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Mock
     private RegisterService registerService;
@@ -27,9 +45,26 @@ class RegisterControllerTest {
     @InjectMocks
     private RegisterController registerController;
 
+    MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
+        assertThat(registerController).isNotNull();
         initMocks(this);
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+    }
+
+    @Test
+    @WithMockUser
+    void shouldGetRegisterPage() throws Exception {
+        mockMvc.perform(get("/regist"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(model().attributeExists("message"));
     }
 
     @Test
